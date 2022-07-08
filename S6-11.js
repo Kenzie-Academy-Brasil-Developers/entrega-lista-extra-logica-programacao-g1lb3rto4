d1456a4ec960a608fc63faa50133c3977a5429bf
Gilberto <gilbertooliveira266@gmail.com>
2022-07-08 12:56:28 -0300

    correcao entrega S6-11 agora vai

--- S6-11.js
+++ S6-11.js
@@ -1,6 +1,6 @@
 // exercicico 1 e 2
 let totalFixo = 1000
-let valorVendas = 500
+let valorVendas = 1450
 
 function calculateSalary(x,y){
   let total = 0
@@ -9,30 +9,106 @@ function calculateSalary(x,y){
     }
 
     else if(y > 1200){
-    total = x + ((y / 100 )* 5)
+    let a = ((y / 100 )* 3)
+    let b = (((y - 1200)/100)*5)
+    total = x + a + b
     }
     return total
 }
  console.log((calculateSalary(totalFixo,valorVendas)))
 
 
+// notas de 200
 
-let valorSaque = [200, 200, 100, 50]
+let total = calculateSalary(totalFixo,valorVendas)
+let nota200 = 200
+
+function notas(x,y){
+
+  let contador200 = 0
+
+for(let i = 0; i < x; i++){
+      contador200 = i / y
+  }
+  return parseInt(contador200)
+}
+
+console.log(notas(total, nota200))
+
+let totaDe200 = notas(total, nota200) * 200
+
+// notas de 10
+
+let sobrasDe200 = total - totaDe200
+
+let nota10 = 10
+
+function notas(q,w){
+
+  let contador10 = 0
+
+for(let i = 0; i < q; i++){
+      contador10 = i / w
+  }
+  return parseInt(contador10)
+}
+
+console.log(notas(sobrasDe200, nota10))
+
+
+// notas de 5
+
+let total10 = notas(sobrasDe200, nota10)
+
+let sobraNotas = sobrasDe200 - (total10 * 10) 
+
+let nota5 = 5
+
+function notas(a,b){
+
+  let contador5 = 0
+
+for(let i = 0; i < a; i++){
+      contador5 = i / b
+  }
+  return parseInt(contador5)
+}
+
+console.log(notas(sobraNotas, nota5))
+
+// troco
+
+let valor5 = notas(sobraNotas, nota5)
+
+let troco = sobraNotas - (valor5 * 5)
+
+console.log("Valor do troco " + troco)
+
+
+//// funcao apos o saque 
+
+let duzentos = nota200
+
+let cem = nota200 / 2
+
+let cinquenta = nota200 / 4
+
+let quantidade = [1,2,1]
+
+let valorSaque = (quantidade[0] * duzentos + (quantidade[1] * cem + (quantidade[2] * cinquenta)))
+
+console.log("total saque " + valorSaque + ".00 reais")
 
 function cashMachine(z){
-    let resultado = 0
   
     let total = calculateSalary(totalFixo,valorVendas)
 
-    for(let i = 0; i < z.length; i++) {
-      resultado += z[i]
-    }
-    return total - resultado 
+    return "Sobra depois do saque: " + (total - z) + " reais"
   }
   console.log(cashMachine(valorSaque))
 
 
-
+  
 // exercicicio 3
 
 let estoque = 6
