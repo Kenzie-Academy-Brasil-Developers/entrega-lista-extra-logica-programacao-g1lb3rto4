2af9f3f94c8a434ea1adedcac3b7d93ae8b6276d
Gilberto <gilbertooliveira266@gmail.com>
2022-07-09 01:36:24 -0300

    correcao entrega S6-11

--- S6-11.js
+++ S6-11.js
@@ -1,41 +1,38 @@
 // exercicico 1 e 2
-let totalFixo = 1000
-let valorVendas = 1450
-
-function calculateSalary(x,y){
+function calculateSalary(valorFixo, valorVendas){
   let total = 0
-  if(y <= 1200){
-    total = x + ((y / 100 )* 3)
+  if(valorVendas <= 1200){
+    total = valorFixo + ((valorVendas / 100 )* 3)
     }
 
-    else if(y > 1200){
-    let a = ((y / 100 )* 3)
-    let b = (((y - 1200)/100)*5)
-    total = x + a + b
+    else if(valorVendas > 1200){
+    let a = ((1200 / 100 )* 3)
+    let b = (((valorVendas - 1200)/100)*5)
+    total = valorFixo + a + b
     }
     return total
 }
- console.log((calculateSalary(totalFixo,valorVendas)))
+ console.log((calculateSalary(1000,1450)))
 
 
 // notas de 200
 
-let total = calculateSalary(totalFixo,valorVendas)
+let total = calculateSalary(1000,1450)
 let nota200 = 200
 
-function notas(x,y){
+function notas(valorTotal, notaDuzentos){
 
   let contador200 = 0
 
-for(let i = 0; i < x; i++){
-      contador200 = i / y
+for(let i = 0; i < valorTotal; i++){
+      contador200 = i / notaDuzentos
   }
   return parseInt(contador200)
 }
 
 console.log(notas(total, nota200))
 
-let totaDe200 = notas(total, nota200) * 200
+let totaDe200 = notas(total, nota200) * nota200
 
 // notas de 10
 
@@ -43,12 +40,12 @@ let sobrasDe200 = total - totaDe200
 
 let nota10 = 10
 
-function notas(q,w){
+function notas(sobrasDuzentos, notaDez){
 
   let contador10 = 0
 
-for(let i = 0; i < q; i++){
-      contador10 = i / w
+for(let i = 0; i < sobrasDuzentos; i++){
+      contador10 = i / notaDez
   }
   return parseInt(contador10)
 }
@@ -58,18 +55,16 @@ console.log(notas(sobrasDe200, nota10))
 
 // notas de 5
 
-let total10 = notas(sobrasDe200, nota10)
-
-let sobraNotas = sobrasDe200 - (total10 * 10) 
+let sobraNotas = sobrasDe200 - (notas(sobrasDe200, nota10) * nota10) 
 
 let nota5 = 5
 
-function notas(a,b){
+function notas(SobraNotas, notasCinco){
 
   let contador5 = 0
 
-for(let i = 0; i < a; i++){
-      contador5 = i / b
+for(let i = 0; i < SobraNotas; i++){
+      contador5 = i / notasCinco
   }
   return parseInt(contador5)
 }
@@ -78,69 +73,57 @@ console.log(notas(sobraNotas, nota5))
 
 // troco
 
-let valor5 = notas(sobraNotas, nota5)
-
-let troco = sobraNotas - (valor5 * 5)
+let troco = sobraNotas - (notas(sobraNotas, nota5) * nota5)
 
 console.log("Valor do troco " + troco)
 
 
 //// funcao apos o saque 
 
-let duzentos = nota200
+let quantidade = [1, 2, 1]
 
-let cem = nota200 / 2
+let Saque = (quantidade[0] * nota200 + (quantidade[1] * (nota200 / 2) + (quantidade[2] * (nota200 / 4))))
 
-let cinquenta = nota200 / 4
+function cashMachine(valorSaque, valorFixo, valorVendas){
 
-let quantidade = [1,2,1]
+  let total = calculateSalary(valorFixo,valorVendas)
+  return "Sobra depois do saque: " + (total - valorSaque) + " reais"
+}
 
-let valorSaque = (quantidade[0] * duzentos + (quantidade[1] * cem + (quantidade[2] * cinquenta)))
-
-console.log("total saque " + valorSaque + ".00 reais")
-
-function cashMachine(z){
-  
-    let total = calculateSalary(totalFixo,valorVendas)
-
-    return "Sobra depois do saque: " + (total - z) + " reais"
-  }
-  console.log(cashMachine(valorSaque))
+console.log(cashMachine(Saque, 1000, 1450))
+console.log("total saque " + Saque + ".00 reais")
 
 
-  
 // exercicicio 3
 
 let estoque = 6
 let quantidadeMaxima = 8
 let quantidadeMinima = 0
 
-function calculateStock(a,b,c){
+function calculateStock(estoque, maxima, minima){
 
- let quantidadeMedia = ((b + c)/2)
+ let quantidadeMedia = ((maxima + minima)/2)
 
-    if(a >= quantidadeMedia){
+    if(estoque >= quantidadeMedia){
       return `Não efetuar compra`
     }
-    else if(a < quantidadeMedia){
+    else if(estoque < quantidadeMedia){
       return `Efetuar compra`
   
     }
   }
 console.log(calculateStock(estoque,quantidadeMaxima,quantidadeMinima))
 
-
-
 // exercicio 4
 let anoAtual = 2022
 let anoNascimento = 1998
 
-function calculateAge(x,y){
-if(idade = x - y){
+function calculateAge(ano, nascimento){
+if(idade = ano - nascimento){
   return `Eu tenho ${idade} anos, ou ${idade * 12} meses de idade, ou ${idade * 365} dias de idade, ou ${idade * 52} semanas de idade`
 }
 }
-console.log(calculateAge(anoAtual,anoNascimento))
+console.log(calculateAge(anoAtual, anoNascimento))
 
 //exercicio 5
 let matrizQuadrada =[
@@ -148,10 +131,10 @@ let matrizQuadrada =[
   [4, 5, 6],
   [7, 8, 9]
 ]
-function getDiagonal(a){
+function getDiagonal(matriz){
  let resultado = []
-for(let x = 0; x < a.length; x++){
-  resultado.push(a[x][x])
+for(let x = 0; x < matriz.length; x++){
+  resultado.push(matriz[x][x])
   
 }
 return resultado 
